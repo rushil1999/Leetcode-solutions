0133c37f9eeaa40e01c3c10bf2dd84b4fb88d092
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-08-12 15:05:36 -0700

    Time: 110 ms (57.28%), Space: 17.3 MB (37.98%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -4,18 +4,9 @@ class Solution:
         for time in timePoints:
             hour = int(time[0:2])
             minute = int(time[3:5])
-            print(hour, minute)
             totalMinutes = hour*60 + minute
-            
             minList.append(totalMinutes)
-        print(minList)
         minList.sort()
-#         print(minList)
-#         print(abs(minList[1][0]-minList[0][1]))
-        
-#         print(abs(minList[1][0]- minList[0][0]))
-#         print(abs(minList[1][1]- minList[0][0]))
-#         print(abs(minList[1][1]- minList[0][1]))
         minVal = float("inf")
         for i in range(1, len(minList)):
             val = minList[i] - minList[i-1]
