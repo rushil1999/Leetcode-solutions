580b8d3f6783a933f852354c109d363c281ae8d9
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-08-13 20:38:41 -0700

    Time: 364 ms (5.04%), Space: 14.2 MB (16.21%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,5 +1,6 @@
 class Solution:
-    
+    def __init__(self):
+        self.map = {}
     def issubsequence(self, s1, s2):
  
         n,m = len(s1),len(s2)
@@ -8,7 +9,7 @@ class Solution:
             if (s1[i] == s2[j]):
                 i += 1
             j += 1
-     
+        self.map[s1] = True
         return i == n
     
     def shortestWay(self, source: str, target: str) -> int:
@@ -23,6 +24,9 @@ class Solution:
             if (target[index] not in source):
                 return -1
             substr = dp2[index-1] + target[index]
+            if(substr in self.map):
+                dp1[index] = dp1[index-1]
+                dp2[index] = substr
             if(self.issubsequence(substr, source) == True):
                 dp1[index] = dp1[index-1]
                 dp2[index] = substr
