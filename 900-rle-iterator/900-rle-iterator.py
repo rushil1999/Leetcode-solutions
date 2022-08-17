07b73cba5e7e7125ad05412393a7cb17cd14faaa
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-08-17 16:30:17 -0700

    Time: 183 ms (5.52%), Space: 14.7 MB (28.59%) - LeetHub

--- 900-rle-iterator/900-rle-iterator.py
+++ 900-rle-iterator/900-rle-iterator.py
@@ -9,10 +9,8 @@ class RLEIterator:
                 continue
             self.rangeMap[(prev, prev+encoding[index]-1)] = encoding[index+1]
             prev = prev+encoding[index]
-        print(self.rangeMap)
     def next(self, n: int) -> int:
         self.pointer += (n)
-        
         for key in self.rangeMap:
             if(self.pointer-1 >= key[0] and self.pointer-1 <= key[1]):
                 return self.rangeMap[key]
