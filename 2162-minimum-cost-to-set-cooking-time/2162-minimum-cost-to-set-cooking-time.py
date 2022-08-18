c9061c4144654cbc9e4ad5eae256ce9ed5069bf7
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-08-17 17:01:59 -0700

    Time: 34 ms (91.30%), Space: 13.9 MB (91.43%) - LeetHub

---
+++ 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.py
@@ -0,0 +1,52 @@
+class Solution:
+    
+    def createTimeArray(self,minutes, seconds):
+        time = []
+        time.append(minutes//10)
+        time.append(minutes%10)
+        time.append(seconds//10)
+        time.append(seconds%10)
+        
+        for index in range(0,4):
+            if(time[index] > 0):
+                break
+        time = time[index: len(time)]
+        return time
+    
+    def minCostSetTime(self, startAt: int, moveCost: int, pushCost: int, targetSeconds: int) -> int:
+        timeConfigurations = []
+        
+        minutes = targetSeconds//60
+        seconds = targetSeconds%60
+        
+        if(minutes > 99):
+            minutes = 99
+            seconds = seconds + 60
+        timeConfigurations.append(self.createTimeArray(minutes, seconds))
+        
+        if(seconds + 60 <= 99 and minutes > 0):
+            minutes = minutes - 1
+            seconds = seconds + 60
+            if(minutes > 99):
+                minutes = 99
+                seconds = seconds + 60
+            timeConfigurations.append(self.createTimeArray(minutes, seconds))
+            
+            
+        print(timeConfigurations)
+        minCost = float("inf")
+        
+        for configuration in timeConfigurations:
+            currentPosition = startAt
+            cost = 0
+            for unit in configuration:
+                # print(unit)
+                if(currentPosition != unit):
+                    currentPosition = unit
+                    cost += moveCost
+                cost += pushCost
+            # print(configuration, cost)
+            if(cost < minCost):
+                minCost = cost
+                
+        return minCost
