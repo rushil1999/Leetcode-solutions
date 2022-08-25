ad3b9eb90a0fd9e0f6b5da32181d98f3e5d2dd07
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-08-25 16:40:27 -0700

    Time: 424 ms (47.53%), Space: 14.4 MB (49.66%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,27 @@
+class Solution:
+    
+    def __init__(self):
+        self.stepsMap = {}
+    
+    def calculatePower(self, num):
+        if(num == 1):
+            return 0
+        if(num in self.stepsMap):
+            return self.stepsMap[num]
+        if(num%2 == 0):
+            val = num/2
+        else:
+            val = (3*num)+1
+        power = self.calculatePower(val) + 1
+        self.stepsMap[num] = power
+        return power
+    
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        numPowerList = []
+        for num in range(lo, hi+1):
+            power = self.calculatePower(num)
+            numPowerList.append((num,power))
+        finalList = sorted(numPowerList, key=lambda x: x[1])
+        print(finalList)
+        
+        return finalList[k-1][0]
