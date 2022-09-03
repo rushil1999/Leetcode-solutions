d75cdc38cd05cef0fc5d1444dcb15aae922b20a8
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-03 14:03:10 -0700

    Time: 105 ms (69.78%), Space: 40.3 MB (60.45%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int i, j, ans = 1, flag = 0;
-        int val;
-        for(i=0;i<arr.size();i++){
-            for(j=i+1;j<arr.size();j++){
-                // cout<<"Start "<<i<<" "<<j<<"\n";
-                // cout<<"IN "<<arr[j-1]<<" "<<arr[j]<<" "<<flag<<"\n";
-                if(((arr[j] > arr[j-1]) && (flag == -1 || flag == 0))){
-                    // cout<<"Went in 1\n";
-                    flag = 1;
-                }
-                else if(((arr[j-1]> arr[j]) && (flag == 1 || flag == 0) )) {
-                    // cout<<"Went in 2\n";
-                    flag = -1;
+        int i, j=0, ans = 1, flag = 0;
+        int val, start=0;
+        for(j=start+1;j<arr.size();j++){
+            // if(flag == 0){
+            //     start = j;
+            // }
+            // cout<<"Start " <<start<<" "<<j<<"\n";
+            if(((arr[j] > arr[j-1]) && (flag == -1 || flag == 0))){
+                flag = 1;
+            }
+            else if(((arr[j-1]> arr[j]) && (flag == 1 || flag == 0) )) {
+                flag = -1;
+            }
+            else{
+                // cout<<"Caught "<<j<<"\n";
+                val = (j-1)-start+1;
+                if(j-start == 1){
+                    // cout<<"Hey\n";
+                    start = j;
                 }
                 else{
-                    // cout<<"Caught "<<i<<" "<<j<<" "<<flag<<"\n";
-                    val = (j-1)-i+1;
-                    if(j-i == 1){
-                        i = j-1;
-                    }
-                    else{
-                        i = j-2;
-                    }
-                    flag = 0;
-                    if(ans < val){
-                        ans = val;
-                    }
-                    break;
+                    start = j-1;
                 }
-            }
-            if(flag == 1 || flag == -1){
-                val = (j-1)-i+1; 
+                j = start;
+                flag = 0;
                 if(ans < val){
-                        ans = val;
+                    ans = val;
                 }
+                // break;
             }
         }
+        if(flag == 1 || flag == -1){
+            // cout<<"Final "<<start<<" "<<j<<"\n";
+            val = (j-1)-start+1; 
+            if(ans < val){
+                    ans = val;
+            }
+        }
+        
         return ans;
     }
 };
