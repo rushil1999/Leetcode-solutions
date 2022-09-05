9263752c58c57455a21a7cfe7c260e4155b12b0a
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-05 15:08:09 -0700

    Time: 464 ms (28.46%), Space: 16.4 MB (95.74%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -8,21 +8,14 @@ class BrowserHistory:
         self.urls = self.urls[0:self.currentIndex+1]
         self.urls.append(url)
         self.currentIndex = len(self.urls)-1
-        # print(self.urls)
         return None
 
     def back(self, steps: int) -> str:
         index = self.currentIndex
-        # while(index >=0 and steps > 0):
-        #     index -= 1
-        #     steps -= 1
         self.currentIndex = 0 if self.currentIndex - steps < 0 else self.currentIndex - steps
         return self.urls[self.currentIndex]
     def forward(self, steps: int) -> str:
         index = self.currentIndex
-        # while(index < len(self.urls) and steps > 0):
-        #     index += 1
-        #     steps -= 1
         self.currentIndex = len(self.urls)-1 if self.currentIndex + steps >=  len(self.urls) else self.currentIndex + steps
         return self.urls[self.currentIndex]
 
