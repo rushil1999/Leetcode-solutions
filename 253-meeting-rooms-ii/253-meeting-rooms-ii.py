0cf29239c5097f07f446d928c18c66897382f306
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-07 12:52:15 -0700

    Time: 83 ms (93.06%), Space: 17.3 MB (98.65%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: x[0])
-        print(intervals)
+        # print(intervals)
         meetingList = [intervals[0][1]]
         heapq.heapify(meetingList)
         for index in range(1, len(intervals)):
@@ -11,5 +11,5 @@ class Solution:
                 heapq.heappush(meetingList, top)
             else:
                 heapq.heappush(meetingList, intervals[index][1])
-        print(meetingList)
+        # print(meetingList)
         return len(meetingList)
