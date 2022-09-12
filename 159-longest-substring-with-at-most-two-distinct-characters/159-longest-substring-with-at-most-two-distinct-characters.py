e0d065093c0b6d53d9bf3b68beb6eaa824d550d3
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-12 14:55:50 -0700

    Time: 422 ms (83.12%), Space: 14.7 MB (86.74%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -8,12 +8,9 @@ class Solution:
             if(len(countMap) == 2):
                 if(s[right] not in countMap):
                     index = min(countMap.values())
-                    leftIncrement = index + 1
-                    del countMap[s[index]]
-                    left = leftIncrement
-                    
+                    left = index + 1
+                    del countMap[s[index]]                    
             countMap[s[right]] = right
-            # print(left, right, right-left+1, countMap)
             res = max(res, right-left+1)
             right += 1
         return res
