9e14ab5d4f2ea97a8e2ec0fa0079b6b121e0c269
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-12 15:17:33 -0700

    Time: 1642 ms (38.10%), Space: 48 MB (41.10%) - LeetHub

--- 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
+++ 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
@@ -1,30 +1,11 @@
 class Solution:
     def countDistinct(self, s: str) -> int:
-        m = {}
+        substringSet = set()
+        for i in range(0, len(s)):
+            for j in range(i+1, len(s)+1):
+                st = s[i:j]
+                # print(st)
+                substringSet.add(st)
+        return len(substringSet)
         
-        def func(st, m):
-            # print(st)
-            if(len(st) == 0 or st in m):
-                return
-            elif(len(st) == 1):
-                m[st] = True
-                return
-            
-            if(st[1:len(st)] not in m):
-                func(st[1:len(st)],m)
-            if(st[0:len(st)-1] not in m):
-                func(st[0:len(st)-1],m)
-            if(st[1:len(st)-1] not in m):
-                func(st[1:len(st)-1],m)
-            m[st] = True
-        
-        func(s, m)
-#         temp = []
-#         for keys in m:
-#             # print(keys)
-#             temp.append(keys)
-        
-#         temp.sort()
-#         print(temp)
-        # print(m, len(m))
-        return(len(m)) 
+
