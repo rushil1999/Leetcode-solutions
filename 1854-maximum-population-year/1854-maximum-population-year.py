29b250d74983457be196002ceb4e6dcadb7901a3
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-17 13:27:01 -0700

    Time: 92 ms (18.88%), Space: 14 MB (5.51%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        populationMap = {}
+        for log in logs:
+            for year in range(log[0], log[1]):
+                populationMap[year]= populationMap[year] + 1 if year in populationMap else 1
+        print(populationMap)
+        maxCount = 0
+        maxYear = float("inf")
+        for year in populationMap:
+            if(populationMap[year] > maxCount):
+                print(year)
+                maxCount = populationMap[year]
+                maxYear = year
+            elif(populationMap[year] == maxCount):
+                maxYear = min(year, maxYear)
+        return maxYear
