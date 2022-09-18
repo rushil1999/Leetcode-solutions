97556c92b8f854b27920895b7b2b8b74620f49cc
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-18 15:07:31 -0700

    Time: 908 ms (8.68%), Space: 30.8 MB (88.88%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        indexList = arr.copy()
+        rankMap = {}
+        indexList.sort()
+        if(len(arr) == 0):
+            return []
+        rankMap[indexList[0]] = 1
+        for index in range(1,len(indexList)):
+            rankMap[indexList[index]] = rankMap[indexList[index-1]]+1 if indexList[index] != indexList[index-1] else rankMap[indexList[index-1]]
+        print(rankMap)
+        finalList = []
+        for element in arr:
+            finalList.append(rankMap[element]) 
+        return finalList
