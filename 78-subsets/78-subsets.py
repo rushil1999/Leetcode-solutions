c5e516b69fec40a1939a94dae59a420b9d19fafc
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-21 14:29:22 -0700

    Time: 57 ms (48.55%), Space: 14.2 MB (35.92%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,28 +1,30 @@
 class Solution:
     
-    def helper(self, nums):
-        final = []
-        if(len(nums) == 1):
-            final.append([nums[0]])
-            print("Initial",final)
-            return final
-        else:
-            print("Before recurring", nums)
-            temp = self.helper(nums[0:len(nums)-1])
-            print("Nums",nums, temp)
-            final = temp[:]
-            final.append([nums[len(nums)-1]])
-            for element in temp:
-                print("Element",element)
-                newElement = element[:]
-                newElement.append(nums[len(nums)-1])
-                print("New Element", newElement)
-                final.append(newElement)
-            print(final)
-        return final
+    # def helper(self, nums):
+    #     final = []
+    #     if(len(nums) == 1):
+    #         final.append([nums[0]])
+    #         return final
+    #     else:
+    #         temp = self.helper(nums[0:len(nums)-1])
+    #         final = temp[:]
+    #         final.append([nums[len(nums)-1]])
+    #         for element in temp:
+    #             newElement = element[:]
+    #             newElement.append(nums[len(nums)-1])
+    #             final.append(newElement)
+    #     return final
     
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        final = self.helper(nums)
-        final.append([])
+        final = []
+        for num in nums:
+            aux = []
+            for element in final:
+                temp = element[:]
+                temp.append(num)
+                aux.append(temp)
+            final = final+ aux
+            final.append([num])
         
+        final.append([])
         return final
