084be97a2db3017321ec08a808090d895ca5ef2c
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-23 15:01:37 -0700

    Time: 129 ms (15.17%), Space: 14 MB (64.25%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,18 +1,22 @@
+from collections import Counter
+from heapq import heappush, heappop
+
+
+class Pair:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+
+    def __lt__(self, p):
+        return self.freq < p.freq or (self.freq == p.freq and self.word > p.word)
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        l1 = []
-        heapq.heapify(l1)
-        wordMap = {}
-        for word in words:
-            wordMap[word] = 1 if word not in wordMap else wordMap[word]+1
-
-        for word in wordMap:
-            heapq.heappush(l1, ((-1*wordMap[word]), word))
-        # print(list(l1))
-        final = []
-        while(len(final) < k):
-            top = heapq.heappop(l1)
-            final.append(top[1])
-        return final
-                    
-                    
+        cnt = Counter(words)
+        h = []
+        for word, freq in cnt.items():
+            heappush(h, Pair(word, freq))
+            if len(h) > k:
+                heappop(h)
+        return [p.word for p in sorted(h, reverse=True)]
