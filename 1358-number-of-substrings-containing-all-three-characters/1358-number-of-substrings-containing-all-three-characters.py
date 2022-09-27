d119ef17498a6bd240c17094306cb14a986bc391
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-27 14:37:29 -0700

    Time: 944 ms (7.20%), Space: 14.5 MB (9.65%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,42 +1,21 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        characterSet = set(['a','b','c'])
-        freqMap = {}
-        # for char in s:
-        #     characterSet.add(char)
-        
-        left = 0
-#         for index in range(len(s)):
-#             freqMap[s[index]] = 1 if s[index] not in freqMap else freqMap[s[index]]+1
-            
-#             if(len(freqMap) == len(characterSet)):
-#                 index+1
-#                 break
-        
+        left, right, prevLeft = 0,0, 0
+        charMap = {s[0]:1}
         count = 0
-        # print("Length of character set", len(characterSet))
-        left, right = 0,0
-        
-        print(left, right, count, len(characterSet), len(freqMap))
-        while(left<=right):
-            freqMap[s[right]] = 1 if s[right] not in freqMap else freqMap[s[right]]+1
-            # print("Outer Loop", left, right, freqMap)
-            while(len(freqMap) ==len(characterSet) ):
-                count += len(s)- right
-                # print("Caught", left, right, s[left:right+1], count, freqMap)
-                freqMap[s[left]] -=1
-                if(freqMap[s[left]] == 0):
-                    del freqMap[s[left]]
+        while(left<=right ):
+            if(len(charMap) < 3 and right < len(s)-1):
+                right += 1
+                charMap[s[right]] = 1 if s[right] not in charMap else charMap[s[right]]+1
+            elif(len(charMap) == 3):
+                count += len(s) - right
+                
+                charMap[s[left]] -=1
+                if(charMap[s[left]] == 0):
+                    del charMap[s[left]]
                 left += 1
-                
-            right+= 1
-            if(right >= len(s) ):
-                break
-                
-                
-                
-        return count
-            
+            else: left+=1
+        return count    
         
         
             
