01c3734882cc85a78c8d9d5331118d7c3ceb7f6b
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-09-27 14:56:38 -0700

    Time: 132 ms (25.16%), Space: 29.5 MB (10.90%) - LeetHub

---
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def treeTraversal(self, node, visited):
+        if(node == None):
+            return '#'
+        pathString = str(node.val)
+        leftChildPathString = self.treeTraversal(node.left, visited)
+        pathString += "(" + leftChildPathString + ")"
+        rightChildPathString = self.treeTraversal(node.right, visited)
+        pathString += "(" + rightChildPathString + ")"
+        
+        if(pathString in visited):
+            print(pathString, node)
+        visited[pathString] = (node, 1) if pathString in visited else (node, 0)
+        
+        return pathString
+        
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        final = []
+        visited = {}
+        self.treeTraversal(root, visited)
+        # print(visited)
+        for key in visited:
+            if(visited[key][1] == 1):
+                final.append(visited[key][0])
+        return final
