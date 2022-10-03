c7389efd1a36f74cc052547d4d9d79f30d5dd8c2
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-10-03 16:54:29 -0700

    Time: 121 ms (35.30%), Space: 13.9 MB (78.54%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        dp = []
+        for num in range(1, n+1):
+            dp.append(num)
+        print(dp)
+        index = 0
+        while(len(dp) > 1):
+            index = ((index+k-1)%len(dp))
+            # print("Iteration", index)
+            # print("Plaer leaving the game", index, dp[index])
+            dp = dp[0:index]+dp[index+1:len(dp)]
+            # index += 1
+            # print("After Removal", dp, index)
+        return dp[0]
