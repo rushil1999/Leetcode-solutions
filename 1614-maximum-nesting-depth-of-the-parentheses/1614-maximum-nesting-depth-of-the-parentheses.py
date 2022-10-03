0460fb0caa9a460d58bfff4732e2c207ca3372bf
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-10-03 10:51:41 -0700

    Time: 29 ms (97.18%), Space: 13.9 MB (9.87%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -10,11 +10,8 @@ class Solution:
                 maxDepth = max(maxDepth, currentDepth)
             elif(character == ')'):
                 top = stack[-1]
-                if(top != "("):
-                    return -1
-                else:
-                    stack = stack[0:len(stack)-1]
-                    currentDepth -= 1
+                stack = stack[0:len(stack)-1]
+                currentDepth -= 1
         if(currentDepth > 0):
             maxDepth = max(currentDepth, maxDepth)
         return maxDepth
