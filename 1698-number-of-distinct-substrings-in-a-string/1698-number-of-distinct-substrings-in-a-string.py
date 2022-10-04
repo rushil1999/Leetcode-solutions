55e2b69f61f1fe78f94b113e42bd53d0d3a6a7d7
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-10-03 18:23:53 -0700

    Time: 1775 ms (42.78%), Space: 48 MB (41.29%) - LeetHub

--- 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
+++ 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def countDistinct(self, s: str) -> int:
         # substringSet = set()
-        m = {}
+        m = set([])
         for i in range(0, len(s)):
             for j in range(i+1, len(s)+1):
                 st = s[i:j]
                 # print(st)
                 if(st not in m):
-                    m[st] = True
+                    m.add(st)
         return len(m)
         
 
