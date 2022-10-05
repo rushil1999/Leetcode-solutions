257c27f57bec1dd28675657b6cde64e063b673da
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-10-05 16:30:59 -0700

    Time: 1691 ms (19.47%), Space: 62 MB (51.78%) - LeetHub

--- 716-max-stack/716-max-stack.py
+++ 716-max-stack/716-max-stack.py
@@ -7,14 +7,11 @@ class MaxStack:
         self.counter = 1
         
     def push(self, x: int) -> None:
-        # print("Operation Push")
         self.stack.append((self.counter, x))
         heapq.heappush(self.heapList, (-x, -self.counter))
         self.counter += 1
         
-        # print(self.stack, self.heapList)
     def pop(self) -> int:
-        # print("Operation Pop")
         while(len(self.stack) > 0 and self.stack[-1][0] in self.removed):
             self.stack = self.stack[0:len(self.stack)-1]
         poppedElement = self.stack[-1]
@@ -22,23 +19,18 @@ class MaxStack:
         self.stack = self.stack[0:len(self.stack)-1]
         return poppedElement[1]
     def top(self) -> int:
-        # print("Operation Top")
         while(len(self.stack) > 0 and self.stack[-1][0] in self.removed):
             self.stack = self.stack[0:len(self.stack)-1]
-        # print(self.stack)
         return self.stack[-1][1] if len(self.stack) > 0 else []
     def peekMax(self) -> int:
-        # print("Operation PeekMax")
         while(len(self.heapList) > 0 and -self.heapList[0][1] in self.removed):
             heapq.heappop(self.heapList)
         return -self.heapList[0][0] if len(self.heapList) > 0 else []
     def popMax(self) -> int:
-        # print("Operation PopMax")
         while(len(self.heapList) > 0 and -self.heapList[0][1] in self.removed):
             heapq.heappop(self.heapList)
         poppedElement = heapq.heappop(self.heapList)
         self.removed[-poppedElement[1]] = True
-        # print(self.stack, self.heapList, self.removed)
         
         return -poppedElement[0]
 # Your MaxStack object will be instantiated and called as such:
