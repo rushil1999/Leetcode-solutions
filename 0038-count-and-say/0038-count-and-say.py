62f1b98c852972a43036f2bcfdb1e37cd8190b76
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-10-15 14:08:53 -0700

    Time: 59 ms (80.63%), Space: 14.1 MB (15.89%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,9 +1,30 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        def recurse(n):
-            if(n == 1):
-                return "1"
-            childStr = recurse(n-1)
+        # def recurse(n):
+        #     if(n == 1):
+        #         return "1"
+        #     childStr = recurse(n-1)
+        #     charStack = []
+        #     for char in childStr:
+        #         if(len(charStack) == 0):
+        #             charStack.append((char, 1))
+        #         else:
+        #             top = charStack[-1]
+        #             if(char == top[0]):
+        #                 charStack[-1] = (charStack[-1][0], charStack[-1][1]+1)
+        #             else:
+        #                 charStack.append((char, 1))
+        #     newStr = ""
+        #     for element in charStack:
+        #         newStr = newStr + str(element[1]) + element[0]
+        #     # print(n, newStr)
+        #     return newStr
+    
+        
+        
+        prev = "1"
+        for i in range(2, n+1):
+            childStr = prev
             charStack = []
             for char in childStr:
                 if(len(charStack) == 0):
@@ -17,9 +38,7 @@ class Solution:
             newStr = ""
             for element in charStack:
                 newStr = newStr + str(element[1]) + element[0]
-            # print(n, newStr)
-            return newStr
-    
-        finalStr = recurse(n)
-        # print(finalStr)
-        return finalStr
+            
+            prev = newStr
+        
+        return prev
