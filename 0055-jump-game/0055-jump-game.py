a0f5768d64e4c581409ae25a7af71dd69ae2f6af
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-11-01 17:50:59 -0700

    Time: 1632 ms (11.08%), Space: 16.9 MB (5.74%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dp = []
+        dp.append([0,nums[0]])
+        for index in range(1, len(nums)):
+            stepsTaken = max(dp[index-1][0], dp[index-1][1])-1
+            stepsAvailable = nums[index] if stepsTaken >= 0 else 0
+            dp.append([ stepsTaken, stepsAvailable])
+        print(dp)
+        return True if dp[-1][0]>=0 else False
