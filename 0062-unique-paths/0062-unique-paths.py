00cfe9795ea50015ad44e5770cf6d7e60b3143d6
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-11-09 19:36:52 -0800

    Time: 43 ms (79.09%), Space: 13.9 MB (31.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,30 +1,25 @@
 class Solution:
-    # Top Down
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1] * n for _ in range(m)]
-        
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        return dp[m-1][n-1]
-    
-    
-    # Bottom Up Recurssive
 #     def uniquePaths(self, m: int, n: int) -> int:
         
 #         memo = {}
-        
 #         def helper(m,n):
 #             if((m,n) in memo):
 #                 return memo[(m,n)]
-#             if(m < 1 or n < 1):
-#                 return 0
 #             if(m == 1 and n == 1):
 #                 return 1
-#             # target = (m-1, n-1)
-#             top = self.uniquePaths(m-1, n)
-#             left = self.uniquePaths(m, n-1)
-#             memo[(m,n)] = top+left
-#             return top+left
-        
+#             if(m < 1 or n < 1):
+#                 return 0
+#             paths = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+#             memo[(m,n)] = paths
+#             return paths 
 #         return helper(m,n)
+
+    def uniquePaths(self, m: int,n :int ) -> int:
+        dp = [[1]*n for num in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+               
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+                
+                
