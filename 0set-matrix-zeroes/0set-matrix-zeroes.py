9ca57e1a1b431377681543ae91f30f300226e921
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-11-10 12:37:26 -0800

    Time: 359 ms, Memory: 14.7 MB - LeetHub

--- 0set-matrix-zeroes/0set-matrix-zeroes.py
+++ 0set-matrix-zeroes/0set-matrix-zeroes.py
@@ -15,24 +15,10 @@ class Solution:
                     matrix[i][0] = 0
                     matrix[0][j] = 0
                     
-        print(matrix)
         for i in range(1,n):
             for j in range(1,m):
-                # print(i,j, matrix[i][0], matrix[0][j])
                 if(matrix[i][0] == 0 or matrix[0][j] == 0):
                     matrix[i][j] = 0
-                    
-        print(matrix)
-        
-        
-#         for j in range(m):
-#             if(matrix[0][j] == 0):
-#                 topRow = True
-                
-                
-#         for i in range(n):
-#             if(matrix[i][0] == 0):
-#                 leftCol = True
                 
         if(topRow == True):
             for j in range(m):
