d44bd75c7b98c4da13d434a85da50f973e73115d
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-11-09 17:17:20 -0800

    Time: 1651 ms (11.20%), Space: 16.6 MB (5.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,19 +1,19 @@
 class Solution:
     # bottom up approach
-    # def canJump(self, nums: List[int]) -> bool:
-    #     dp = []
-    #     dp.append([0,nums[0]])
-    #     for index in range(1, len(nums)):
-    #         stepsTaken = max(dp[index-1][0], dp[index-1][1])-1
-    #         stepsAvailable = nums[index] if stepsTaken >= 0 else 0
-    #         dp.append([ stepsTaken, stepsAvailable])
-    #     return True if dp[-1][0]>=0 else False
+    def canJump(self, nums: List[int]) -> bool:
+        dp = []
+        dp.append([0,nums[0]])
+        for index in range(1, len(nums)):
+            stepsTaken = max(dp[index-1][0], dp[index-1][1])-1
+            stepsAvailable = nums[index] if stepsTaken >= 0 else 0
+            dp.append([ stepsTaken, stepsAvailable])
+        return True if dp[-1][0]>=0 else False
     
     
     # Greedy
-    def canJump(self, nums: List[int]) -> bool:
-        last = len(nums)-1
-        for index in range(last, -1, -1):
-            if(nums[index] + index >= last):
-                last = index
-        return last == 0
+    # def canJump(self, nums: List[int]) -> bool:
+    #     last = len(nums)-1
+    #     for index in range(last, -1, -1):
+    #         if(nums[index] + index >= last):
+    #             last = index
+    #     return last == 0
