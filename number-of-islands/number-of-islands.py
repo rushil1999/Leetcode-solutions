9234d0e6d2c87682b4bf7bb40fd2368ea146fdbf
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2022-11-16 15:00:16 -0800

    Time: 834 ms, Memory: 16.4 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -8,19 +8,19 @@ class Solution:
         cols = len(grid[0])
         
         def traverseIsland(i,j):
-            print(i,j)
+            # print(i,j)
             grid[i][j] = "0"
             if(i+1 < rows  and grid[i+1][j] == "1"):
-                print("R1", i+1, j, grid[i+1][j])
+                # print("R1", i+1, j, grid[i+1][j])
                 traverseIsland(i+1, j)
             if(i-1>=0  and grid[i-1][j] == "1"):
-                print("R2", i-1, j, grid[i-1][j])
+                # print("R2", i-1, j, grid[i-1][j])
                 traverseIsland(i-1, j)
             if(j+1 < cols  and grid[i][j+1] == "1"):
-                print("R3", i, j+1, grid[i][j+1])
+                # print("R3", i, j+1, grid[i][j+1])
                 traverseIsland(i, j+1)
             if(j-1 >=0  and grid[i][j-1] == "1"):
-                print("R4", i, j-1, grid[i][j-1])
+                # print("R4", i, j-1, grid[i][j-1])
                 traverseIsland(i, j-1)
         
         
@@ -30,7 +30,7 @@ class Solution:
             for j in range(cols):
                 # print(i,j, visited[i][j])
                 if( grid[i][j] == "1"):
-                    print("Inside")
+                    # print("Inside")
                     traverseIsland(i,j)
                     count += 1
         return count
