0d0b78a17c5f31720491d3e947079a748d4cde0c
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-03-11 16:50:15 -0800

    Time: 34 ms, Memory: 14 MB - LeetHub

---
+++ plus-one/plus-one.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        finalArray = [0 for _ in range(len(digits)+1)]
+        for i in range(len(digits)-1, -1, -1):
+            val = carry + digits[i]
+            carry = val//10
+            finalArray[i+1] = val%10
+            print("fff", val%10)
+        if(carry >0):
+            finalArray[0] = carry
+        else:
+            finalArray = finalArray[1:len(finalArray)]
+        return finalArray
