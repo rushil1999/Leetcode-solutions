48a7d6f1c695813f52c7ba3dd642fb5c1d728d04
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-04-22 14:06:29 -0700

    Time: 99 ms, Memory: 17.4 MB - LeetHub

---
+++ delete-and-earn/delete-and-earn.py
@@ -0,0 +1,27 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        size = len(nums)
+        count = defaultdict(lambda: 0)
+        for i in range(size):
+            count[nums[i]] += 1
+        arr = []
+        for key in list(count.keys()):
+            arr.append((key, count[key]))
+        arr.sort(key = lambda x: x[0])
+        print(arr)
+        subSize = len(arr)
+        dp = [[0]*2 for _ in range(subSize)]
+        dp[0][0] = 0
+        dp[0][1] = arr[0][0]*arr[0][1]
+        
+        for i in range(1, subSize):
+            currentVal = arr[i][0]
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1])
+            if(currentVal-arr[i-1][0] == 1):
+                dp[i][1] = currentVal * arr[i][1] + dp[i-1][0]
+            else:
+                dp[i][1] = currentVal*arr[i][1] + dp[i][0]
+        print(dp)
+        return max(dp[subSize-1][0], dp[subSize-1][1])
+        
+        
