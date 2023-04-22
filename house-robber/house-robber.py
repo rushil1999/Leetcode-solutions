e8d3f262365b053565a56e4ff58ebd97fc9a2c33
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-04-22 13:44:20 -0700

    Time: 32 ms, Memory: 13.9 MB - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -2,7 +2,6 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         size = len(nums)
         dp = [[0] *2 for _ in range(size)]
-        print(dp)
         dp[0][0] = 0
         dp[0][1] = nums[0]
         for i in range(1,size):
