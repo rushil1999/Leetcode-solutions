c48f83add2c18193015203b9eb980459e37c8490
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-04-24 20:48:27 -0700

    Time: 35 ms, Memory: 14.3 MB - LeetHub

---
+++ decode-ways/decode-ways.py
@@ -0,0 +1,40 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        memo = {}
+        def recurse(sr):
+            if(len(sr) == 0):
+                return 0
+            if(sr == "0" or (len(sr)>1 and sr[0] == "0")):
+                return 0
+            elif(len(sr) == 1 and sr != "0"):
+                return 1
+        
+            
+            if(sr in memo):
+                return memo[sr]
+            
+            val1 = sr[1:len(sr)]
+            child1 = recurse(val1)
+            
+            
+            val2 = sr[2:len(sr)]
+            if(int(sr[0:2]) <= 26):
+                child2 = recurse(val2)
+            else:
+                child2 = 0
+            # print("Children", val1, child1, val2, child2)
+            
+            memo[val1] = child1
+            memo[val2] = child2
+            memo[sr] = child1+ child2
+            if(len(sr) == 2 and int(sr)<=26):
+                memo[sr] +=1
+                        
+            return memo[sr]
+        
+        ans = recurse(s)
+        print(memo)
+        # print(ans)
+        return ans
+            
