8ef34172d9b0d902fa58e7ff7f9214e8ffa27a4f
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-10-21 14:16:17 -0700

    Time: 82 ms, Memory: 51 MB - LeetHub

---
+++ valid-palindrome/valid-palindrome.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const isAlphaNum = (char) => {
+    return /^[a-zA-Z0-9]+$/.test(char);
+}
+
+var isPalindrome = function(s) {
+    let parsedString = "";
+    Array.from(s).forEach((char) => {
+        if(isAlphaNum(char) === true){
+            parsedString += char.toLowerCase()
+        }
+    })
+    
+    console.log(parsedString)
+    let left = 0
+    let right = parsedString.length-1
+    
+    while(left <= right) {
+        if(parsedString[left] != parsedString[right]){
+            return false
+        }
+        else{
+            left += 1
+            right -= 1
+        }
+    }
+    return true
+    
+    
+};
