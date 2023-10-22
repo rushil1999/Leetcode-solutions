370619adeec9c7d11ba6721337c38f01fc9908c2
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-10-22 15:59:58 -0700

    Time: 114 ms, Memory: 3 MB - LeetHub

--- minimum-window-substring/minimum-window-substring.go
+++ minimum-window-substring/minimum-window-substring.go
@@ -15,22 +15,16 @@ func checkMapping(sMap map[rune]int, tMap map[rune]int) bool {
 func minWindow(s string, t string) string {
     sMap := make(map[rune]int)
     tMap := make(map[rune]int)
-    
     left := 0
     right := 0
     size := len(s)
     ans := ""
-    
-    
     for _, character := range t {
         tMap[rune(character)] += 1
     }
-    
     for right < size {
         character := rune(s[right])
         sMap[character] += 1
-        // val := checkMapping(sMap, tMap)
-        // fmt.Println(string(character), val)
         if checkMapping(sMap, tMap) {
             for left <= right && checkMapping(sMap, tMap) {
                 chacterToRemove := rune(s[left])
@@ -40,8 +34,6 @@ func minWindow(s string, t string) string {
             if right-(left-1)+1 < len(ans) || len(ans) == 0{
                 ans = s[left-1:right+1]
             }
-            
-            // fmt.Println(left, right, ans, s[right], s[left-1:right+1])
         }
         right += 1
     }
