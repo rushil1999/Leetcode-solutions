cc866decc2e376d0d06818674babd7dc301c3372
Rushil Shah <59569478+rushil1999@users.noreply.github.com>
2023-10-21 22:39:54 -0700

    Time: 26 ms, Memory: 6.9 MB - LeetHub

--- move-zeroes/move-zeroes.go
+++ move-zeroes/move-zeroes.go
@@ -5,19 +5,14 @@ func moveZeroes(nums []int)  {
     i := 0
     target := -1
     for i < size {
-        // fmt.Println("DA", nums[i], target, i)
         if(nums[i] != 0 && target > -1 && target < i ) {
-            // fmt.Println("Caught", nums[i], target)
             nums[target] = nums[i]
             nums[i] = 0
             k:= target+1
-            // fmt.Println("WHereada", k, nums[k])
             for k < size && nums[k] != 0 {
                 k+=1
             }
-            
             target = k
-            // fmt.Println("New Target", target, i)
         } else if nums[i] == 0 {
             if target == -1 {
                 target = i
